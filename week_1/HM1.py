75fa51e73bc2c58892964730d83c0fdeedb1a5c8
kirs53 <50398217+kirs53@users.noreply.github.com>
2022-09-10 11:04:04 +0300

    Add files via upload

--- week_1/HM1.py
+++ week_1/HM1.py
@@ -4,7 +4,7 @@ c = a // 10 % 10
 d = a % 10
 print(b + c + d)
 
-n = int(150)
-hours = n % (60 * 24) // 60
-minutes = n % 60
-print(hours, minutes)
+a = int(150)
+b = a % (60 * 24) // 60
+c = a % 60
+print(b, c)
